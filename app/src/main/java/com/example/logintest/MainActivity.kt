aa8b0afd93df8471a9d0793df08bca9759bca1e4
wellingtondfr <wellingtondfr@gmail.com>
2022-12-12 16:45:46 -0300

    Inserido homepage

--- app/src/main/java/com/example/logintest/MainActivity.kt
+++ app/src/main/java/com/example/logintest/MainActivity.kt
@@ -1,13 +1,43 @@
 package com.example.logintest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputEmail = findViewById<EditText>(R.id.editTextEmailAddress)
+        val inputPassword = findViewById<EditText>(R.id.editTextPassword)
+        val submitLoginButton = findViewById<Button>(R.id.buttonLogin)
+
+        fun showError(inputName: TextView, message: String) {
+            inputName.setError(message)
+            inputName.requestFocus()
+        }
+
+        submitLoginButton.setOnClickListener {
+            val inputEmailValue = inputEmail.text.toString()
+            val inputPasswordValue = inputPassword.text.toString()
+
+            if (inputEmailValue == "gbwellington@hotmail.com" && inputPasswordValue == "1234") {
+                val intent = Intent(this, HomePage::class.java)
+                intent.putExtra("USERNAME", "Wellington")
+                startActivity(intent)
+            } else if (inputEmailValue == "") {
+                showError(inputEmail, "É necessário preencher o email")
+
+            } else if (inputPasswordValue == "") {
+                showError(inputPassword, "É necessário preencher a senha")
+            } else {
+            Toast.makeText(this,"Login ou senha inválidos",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
